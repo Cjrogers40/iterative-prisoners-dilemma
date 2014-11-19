8575135ed7e7a519a70393827b10d7e1202d2e1d
Derk Thomas <16thomas-derrick@users.noreply.github.com>
2014-11-19 10:59:45 -0600

    Update Iterative_Prisoner's_Dilemna.py

--- Iterative_Prisoner's_Dilemna.py
+++ Iterative_Prisoner's_Dilemna.py
@@ -281,16 +281,9 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 7:
         if getting_team_name:
-            return '   '
-        else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were sucker last time
-            else:
-                return 'c' #otherwise collude
+            return 'nipplewaters'
+        else: 'b'
+    #Jesus told me to backstab. So that's what I'm going to do.
 
 
 

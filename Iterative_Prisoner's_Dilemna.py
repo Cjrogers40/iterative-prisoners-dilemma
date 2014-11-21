e5f2822a6d2ffc3e6d149a1122d90e8632525d23
alyssamoses <15moses-alyssa@jonesboroschools.net>
2014-11-21 09:40:05 -0600

    Update Iterative_Prisoner's_Dilemna.py

--- Iterative_Prisoner's_Dilemna.py
+++ Iterative_Prisoner's_Dilemna.py
@@ -406,13 +406,13 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 10:
         if getting_team_name:
-            return '   '
+            return 'Pokemon Master'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)==0: #It's the first round: betray
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
+            elif history[0]=='c' and opponent_history[-1]=='b':
                 return 'b' # betray is they were severely punished last time
             else:
                 return 'c' #otherwise collude
